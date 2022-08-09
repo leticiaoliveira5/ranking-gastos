42227190aab46573e2fb58eeda819efe6a3b3502
leticiaoliveira5 <leoliveira994@gmail.com>
2022-08-09 13:47:23 -0300

    insere validação e teste unitário

--- app/models/deputy.rb
+++ app/models/deputy.rb
@@ -1,2 +1,3 @@
 class Deputy < ApplicationRecord
+  validates :registration_id, presence: true
 end
--- spec/models/deputy_spec.rb
+++ spec/models/deputy_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Deputy, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validation' do
+    it 'is not valid without registration_id' do
+      expect(subject).not_to be_valid
+    end
+
+    it 'is valid with registration_id' do
+      subject.registration_id = 11
+
+      expect(subject).to be_valid
+    end
+  end
 end
