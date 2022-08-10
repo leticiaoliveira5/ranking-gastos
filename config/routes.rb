df5e363e3729270ebfa806e72f10ae9a0ae22b45
leticiaoliveira5 <leoliveira994@gmail.com>
2022-08-10 12:55:39 -0300

    cria rotas e página inicial

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  root 'home#index'
+
+  resources :deputies, only: %i[show]
+
+  resources :expenditures, only: %i[show]
+  
+  namespace :file_upload do
+    get :new
+    post :load_data
+  end
 end
