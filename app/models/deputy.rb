50b0f14dbe95f1f3cfa1d4d4db038c7c436f7bca
leticiaoliveira5 <leoliveira994@gmail.com>
2022-08-10 22:27:17 -0300

    adiciona métodos ao model deputy

--- app/models/deputy.rb
+++ app/models/deputy.rb
@@ -2,4 +2,15 @@ class Deputy < ApplicationRecord
   has_many :expenditures
   
   validates :registration_id, presence: true
+  def amount_spent
+    expenditures.collect(&:net_amount).sum
+  end
+
+  def biggest_expenditure
+    expenditures.collect(&:net_amount).max
+  end
+
+  def picture_url
+    "http://www.camara.leg.br/internet/deputado/bandep/#{registration_id}.jpg"
+  end
 end
