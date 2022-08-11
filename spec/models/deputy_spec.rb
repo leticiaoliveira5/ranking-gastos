5b5f39f5160610633e40876c444fd19e310c469e
leticiaoliveira5 <leoliveira994@gmail.com>
2022-08-11 15:12:05 -0300

    adiciona cobertura para deputy

--- spec/models/deputy_spec.rb
+++ spec/models/deputy_spec.rb
@@ -12,4 +12,33 @@ RSpec.describe Deputy, type: :model do
       expect(subject).to be_valid
     end
   end
+
+  describe '#amount_spent' do
+    it 'retorna o total gasto pelo deputado' do
+      deputy = Deputy.create(registration_id: 123)
+      deputy.expenditures.create(net_amount: 100)
+      deputy.expenditures.create(net_amount: 500)
+
+      expect(deputy.amount_spent).to eq 600
+    end
+  end
+
+  describe '#biggest_expenditure' do
+    it 'retornar o maior gasto do deputado' do
+      deputy = Deputy.create(registration_id: 123)
+      deputy.expenditures.create(net_amount: 100)
+      deputy.expenditures.create(net_amount: 500)
+
+      expect(deputy.biggest_expenditure).to eq 500
+    end
+  end
+
+  describe '#picture_url' do
+    it 'retorna a url com o identificador do deputado' do
+      subject.registration_id = 123456
+
+      expect(subject.picture_url).to be_a String
+      expect(subject.picture_url).to include '123456'
+    end
+  end
 end
