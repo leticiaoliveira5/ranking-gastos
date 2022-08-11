ddcf5b32e4e88e243374702449f6d0f9800ef1e9
leticiaoliveira5 <leoliveira994@gmail.com>
2022-08-10 22:26:35 -0300

    adiciona variáveis para exibir o ranking

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,3 +1,8 @@
 class HomeController < ApplicationController
-  def index; end
+  def index
+    redirect_to file_upload_new_path unless Deputy.count.positive?
+
+    @deputies = Deputy.all
+    @ranking = @deputies.includes(:expenditures).sort_by(&:amount_spent).reverse
+  end
 end
