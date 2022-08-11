79b4eac49ad27c00faca1f5f877a90025d22b29f
leticiaoliveira5 <leoliveira994@gmail.com>
2022-08-10 22:28:00 -0300

    adiciona foreign_key no model

--- app/models/deputy.rb
+++ app/models/deputy.rb
@@ -1,7 +1,10 @@
 class Deputy < ApplicationRecord
-  has_many :expenditures
-  
+  has_many :expenditures,
+           primary_key: :registration_id,
+           foreign_key: :deputy_registration_id,
+           class_name: 'Expenditure'
   validates :registration_id, presence: true
+
   def amount_spent
     expenditures.collect(&:net_amount).sum
   end
